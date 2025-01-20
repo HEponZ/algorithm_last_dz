ad828a8c79dbba9bd68c5bbdf32593d14e8ba083
HEponZ <2titovivan194@gmail.com>
2025-01-20 21:55:45 +0300

    Исправления

--- Car.h
+++ Car.h
@@ -13,17 +13,17 @@ private:
 	double eng_vol;
 	int price;
 public:
-	Car() : Car(string(), 0, 0, 0) {}
-	Car(string name_S, int year_S, double vol_S, int price_S) : name{ name_S }, year{ year_S }, eng_vol{ vol_S }, price{ price_S } {}
+	Car() noexcept : Car(string(), 0, 0, 0) {}
+	Car(string name_S, int year_S, double vol_S, int price_S) noexcept : name{ name_S }, year{ year_S }, eng_vol{ vol_S }, price{ price_S } {}
 
-	string get_name() const { return name; }
-	int get_year() { return year; }
-	double get_vol() { return eng_vol; }
-	int get_price() const { return price; }
-	void set_name(string name_S) { name = name_S; }
-	void set_year(int year_S) { year = year_S; }
-	void set_vol(double vol_S) { eng_vol = vol_S; }
-	int set_price(int price_S) { price = price_S; }
+	string get_name() const noexcept { return name; }
+	int get_year() const noexcept { return year; }
+	double get_vol() const noexcept { return eng_vol; }
+	int get_price() const noexcept { return price; }
+	void set_name(string name_S) noexcept { name = name_S; }
+	void set_year(int year_S) noexcept { year = year_S; }
+	void set_vol(double vol_S) noexcept { eng_vol = vol_S; }
+	int set_price(int price_S) noexcept { price = price_S; }
 
 	friend ostream& operator<<(ostream& my_cout, const Car& car);
 };
